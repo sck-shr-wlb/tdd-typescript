8981bbbdc034822b6738007526d63743e6303197
Karan Sivarat <bomb0069@gmail.com>
2017-03-13 16:33:48 +0700

    move new HelloWorld to beforeEach

--- tests/HelloWorldTest.ts
+++ tests/HelloWorldTest.ts
@@ -3,17 +3,19 @@ import "should"
 import { HelloWorld } from "../src/HelloWorld";
 
 describe("HelloWorld", () => {
+    let tested: HelloWorld;
+
+    beforeEach(() => tested = new HelloWorld());
+
     describe("Say Hi", () => {
         it("should say Hi, karan", () => {
             const expected = "Hi, Karan";
-            let tested: HelloWorld = new HelloWorld();
             const result = tested.sayHi("Karan");
             result.should.be.equal(expected);
         }),
 
         it("should say Hi, bomb0069", () => {
             const expected = "Hi, bomb0069";
-            let tested: HelloWorld = new HelloWorld();
             const result = tested.sayHi("bomb0069");
             result.should.be.equal(expected);
         })
