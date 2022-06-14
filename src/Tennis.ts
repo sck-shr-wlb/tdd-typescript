89b1e7b3545fdc2ad34c2b991bad4a9c304d10c8
hankla.tho <hankla.tho@cpmatch.com>
2022-06-14 09:24:56 +0700

    [add] case player B

--- src/Tennis.ts
+++ src/Tennis.ts
@@ -1,13 +1,25 @@
 export class Tennis{
+  
     playerA: number = 0;
+    playerB: number = 0
 
     aWonPoint() {
         this.playerA++;
     }
+    bWonPoint() {
+       this.playerB++
+    }
     score() {
-        const array: Array<String> = ['Love','15','30','40']
+        const array: Array<String> = ['Love','15','30','40','Win for ']
 
-        return `${array[this.playerA]}-Love` 
+        if (this.playerA == 4) {
+            return `${array[this.playerA]}A`
+        }
+        if (this.playerB == 4) {
+            return `${array[this.playerB]}B`
+        }
+
+        return `${array[this.playerA]}-${array[this.playerB]}` 
     }
     
 }
--- tests/Tennis.test.ts
+++ tests/Tennis.test.ts
@@ -50,4 +50,46 @@ describe("Tennis", () => {
         
 
     })
+
+    it('เมื่อ Player A ได้ 4 แต้ม และ Player A ชนะ', () => {
+        const expectedScore = 'Win for A'
+        
+        const tennis = new Tennis();
+        tennis.aWonPoint();
+        tennis.aWonPoint();
+        tennis.aWonPoint();
+        tennis.aWonPoint();
+
+        const actualScore = tennis.score();
+
+        actualScore.should.be.equal(expectedScore)
+
+    })
+
+    it('เมื่อ Player B ได้แต้ม แต้มต้องเป็น Love-15',()=>{
+        const expectedScore = "Love-15";
+
+        const tennis = new Tennis();
+        tennis.bWonPoint();
+        const actualScore = tennis.score();
+
+        actualScore.should.be.equal(expectedScore);
+
+    })
+
+    it('เมื่อ Player B ได้ 4 แต้ม และ Player B ชนะ', () => {
+        const expectedScore = 'Win for B'
+        
+        const tennis = new Tennis();
+        tennis.bWonPoint();
+        tennis.bWonPoint();
+        tennis.bWonPoint();
+        tennis.bWonPoint();
+
+        const actualScore = tennis.score();
+
+        actualScore.should.be.equal(expectedScore)
+
+    })
+
 });
