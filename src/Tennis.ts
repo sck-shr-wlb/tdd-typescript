eff4938775746631d1b4b3531f69dda7a8b87d1c
tummappht <p.ongartitthichai@gmail.com>
2022-06-15 09:32:43 +0700

    [Added] test case for advantage and win

--- src/Tennis.ts
+++ src/Tennis.ts
@@ -10,16 +10,27 @@ export class Tennis{
        this.playerB++
     }
     score() {
-        const array: Array<String> = ['Love','15','30','40','Win for ']
+        const array: Array<String> = ['Love','15','30','40']
 
-        if (this.playerA == 4) {
-            return `${array[this.playerA]}A`
+        if(this.playerA > 3 || this.playerB > 3) {
+            if(this.playerA - this.playerB >= 2) {
+                return 'Win for A'
+            }
+            if(this.playerB - this.playerA >= 2) {
+                return 'Win for B'
+            }
         }
-        if (this.playerB == 4) {
-            return `${array[this.playerB]}B`
-        }
-        if(this.playerA == 3 && this.playerB == 3) {
-            return 'Deuce'
+
+        if(this.playerA >= 3 || this.playerB >= 3) {
+            if(this.playerA === this.playerB) {
+                return 'Deuce'
+            }
+            if(this.playerA - this.playerB === 1) {
+                return 'Advantage for A'
+            }
+            if(this.playerB - this.playerA === 1) {
+                return 'Advantage for B'
+            }
         }
 
         return `${array[this.playerA]}-${array[this.playerB]}` 
--- tests/TennisDeuce.test.ts
+++ tests/TennisDeuce.test.ts
@@ -19,5 +19,128 @@ describe("Tennis Deuce", () => {
         actualScore.should.be.equal(expectedScore);
 
     })
+
+    it('เมื่อ Score เป็น Deuce ถ้า A ทำแต้มได้ Score จะต้องเป็น Advantage for A',()=>{
+        const expectedScore = "Advantage for A";
+
+       const tennis = new Tennis();
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+       tennis.aWonPoint()
+
+       const actualScore = tennis.score();
+
+        actualScore.should.be.equal(expectedScore);
+
+    })
+
+    it('เมื่อ Score เป็น Deuce ถ้า B ทำแต้มได้ Score จะต้องเป็น Advantage for B',()=>{
+        const expectedScore = "Advantage for B";
+
+       const tennis = new Tennis();
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+       tennis.bWonPoint()
+
+       const actualScore = tennis.score();
+
+        actualScore.should.be.equal(expectedScore);
+
+    })
+
+    it('เมื่อทั้งสองได้แต้ม 4 - 4 Score จะต้องเป็น Deuce',()=>{
+        const expectedScore = "Deuce";
+
+       const tennis = new Tennis();
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+
+       const actualScore = tennis.score();
+
+        actualScore.should.be.equal(expectedScore);
+
+    })
+
+    it('เมื่อทั้งสองได้แต้ม 5 - 5 Score จะต้องเป็น Deuce',()=>{
+        const expectedScore = "Deuce";
+
+       const tennis = new Tennis();
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+
+       const actualScore = tennis.score();
+
+        actualScore.should.be.equal(expectedScore);
+
+    })
+
+    it('เมื่อ A และ B ได้แต้ม 5 - 6 ตามลำดับ Score จะต้องเป็น Advantage for B',()=>{
+        const expectedScore = "Advantage for B";
+
+       const tennis = new Tennis();
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+       tennis.bWonPoint()
+
+       const actualScore = tennis.score();
+
+        actualScore.should.be.equal(expectedScore);
+
+    })
+
+    it('เมื่อ A และ B ได้แต้ม 5 - 7 ตามลำดับ Score จะต้องเป็น Win for B',()=>{
+        const expectedScore = "Win for B";
+
+       const tennis = new Tennis();
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+       tennis.aWonPoint()
+       tennis.bWonPoint()
+       tennis.bWonPoint()
+       tennis.bWonPoint()
+
+       const actualScore = tennis.score();
+
+        actualScore.should.be.equal(expectedScore);
+
+    })
     
 });
