35bac89e03600c2e9d44d08928f3b9a5200a3697
arongkon.chu <arongkon.chu@cpmatch.com>
2022-06-17 08:55:26 +0700

    [Change]: default value constructor

--- src/Tennis.ts
+++ src/Tennis.ts
@@ -1,53 +1,55 @@
-export class Tennis{
-    
-    playerA: number = 0;
-    playerB: number = 0
-    array: Array<String> ;
-    candidateGamescore:number;
-    constructor(arrayScore?: Array<String>) {
-        {arrayScore ? this.array = arrayScore : this.array = ['Love','15','30','40']};
-        this.candidateGamescore = this.array.length -1
+export class Tennis {
+  playerA: number = 0;
+  playerB: number = 0;
+  array: Array<String>;
+  candidateGamescore: number;
+
+  constructor(arrayScore=["Love", "15", "30", "40"]) {
+    this.array = arrayScore;
+    this.candidateGamescore = this.array.length - 1;
+  }
+
+  aWonPoint() {
+    this.playerA++;
+  }
+  bWonPoint() {
+    this.playerB++;
+  }
+
+  score() {
+    if (this.isWin(this.candidateGamescore)) {
+      if (this.playerA - this.playerB >= 2) {
+        return "Win for A";
       }
-    aWonPoint() {
-        this.playerA++;
-    }
-    bWonPoint() {
-       this.playerB++
+      if (this.playerB - this.playerA >= 2) {
+        return "Win for B";
+      }
     }
 
-    score() {
-        
-        
-        if(this.isWin(this.candidateGamescore)) {
-            if(this.playerA - this.playerB >= 2) {
-                return 'Win for A'
-            }
-            if(this.playerB - this.playerA >= 2) {
-                return 'Win for B'
-            }
-        }
-
-        if(this.isDeuce(this.candidateGamescore)) {
-            if(this.playerA === this.playerB) {
-                return 'Deuce'
-            }
-            if(this.playerA - this.playerB === 1) {
-                return 'Advantage for A'
-            }
-            if(this.playerB - this.playerA === 1) {
-                return 'Advantage for B'
-            }
-        }
-
-        return `${this.array[this.playerA]}-${this.array[this.playerB]}` 
-    }
-    
-
-    private isWin(candidateGamescore: number) {
-        return this.playerA > candidateGamescore || this.playerB > candidateGamescore;
+    if (this.isDeuce(this.candidateGamescore)) {
+      if (this.playerA === this.playerB) {
+        return "Deuce";
+      }
+      if (this.playerA - this.playerB === 1) {
+        return "Advantage for A";
+      }
+      if (this.playerB - this.playerA === 1) {
+        return "Advantage for B";
+      }
     }
 
-    private isDeuce(candidateGamescore: number) {
-        return this.playerA >= candidateGamescore || this.playerB >= candidateGamescore;
-    }
+    return `${this.array[this.playerA]}-${this.array[this.playerB]}`;
+  }
+
+  private isWin(candidateGamescore: number) {
+    return (
+      this.playerA > candidateGamescore || this.playerB > candidateGamescore
+    );
+  }
+
+  private isDeuce(candidateGamescore: number) {
+    return (
+      this.playerA >= candidateGamescore || this.playerB >= candidateGamescore
+    );
+  }
 }
