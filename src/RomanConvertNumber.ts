b29a5308390c38490404e8101ef1e295f97cc64a
hankla.tho <hankla.tho@cpmatch.com>
2022-07-04 09:21:29 +0700

    [Refactor] arrRoman , arrArabic =>  arrRomanArabic

--- src/RomanConvertNumber.ts
+++ src/RomanConvertNumber.ts
@@ -1,6 +1,7 @@
 export const convertRomanNumber = (num : number) => {
-    let arrRoman = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I']
-    let arrArabic = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+    let arrRoman = ['M','CM','D','CD','C','XC','L','XL','X','IX']
+    let arrArabic = [1000,900,500,400,100,90,50,40,10,9]
+    let arrRomanArabic = [{roman:'V',arabic:5},{roman:'IV',arabic:4},{roman:'I',arabic:1}]
     let Roman = '';
     
     arrRoman.forEach((element,index) => {
@@ -8,6 +9,12 @@ export const convertRomanNumber = (num : number) => {
             Roman += arrRoman[index];
     }
     });
+
+    arrRomanArabic.forEach(element =>{
+        for(; num >= element.arabic ; num -= element.arabic){
+            Roman += element.roman;
+    }
+    })
     
     return Roman;
 }
