a2ce1912dfb2f5cc1ac044d537856be39b126a75
hankla.tho <hankla.tho@cpmatch.com>
2022-07-04 08:51:57 +0700

    [delete] for = 4

--- src/RomanConvertNumber.ts
+++ src/RomanConvertNumber.ts
@@ -1,22 +1,21 @@
 export const convertRomanNumber = (num : number) => {
-    let arrRoman = ['I']
-    let arrNumber = [1]
+    let arrRoman = ['IV','I']
+    let arrArabic = [4,1]
     let Roman = '';
     for (;num >= 10; num -= 10) {
         Roman += 'X';
     }
+    
     for (;num >= 9; num -= 9) {
         Roman += 'IX';
     }
+ 
     for (;num >= 5; num -= 5) {
         Roman += 'V';
     }
-    for(;num >= 4;num -= 4){
-        Roman += 'IV' ;
-    }
     
     arrRoman.forEach((element,index) => {
-        for(; num >= arrNumber[index] ; num -= arrNumber[index]){
+        for(; num >= arrArabic[index] ; num -= arrArabic[index]){
             Roman += arrRoman[index];
     }
     });
