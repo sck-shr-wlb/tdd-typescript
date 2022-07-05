1ef87df45211a573f21cbc2c2e7b17db593666a5
hankla.tho <hankla.tho@cpmatch.com>
2022-07-05 08:43:23 +0700

    [move] arrRomanArabic ให้มาอยู่นอก function

--- src/RomanConvertNumber.ts
+++ src/RomanConvertNumber.ts
@@ -1,5 +1,6 @@
+let arrRomanArabic = [{roman:'M',arabic:1000},{roman:'CM',arabic:900},{roman:'D',arabic:500},{roman:'CD',arabic:400},{roman:'C',arabic:100},{roman:'XC',arabic:90},{roman:'L',arabic:50},{roman:'XL',arabic:40},{roman:'X',arabic:10},{roman:'IX',arabic:9},{roman:'V',arabic:5},{roman:'IV',arabic:4},{roman:'I',arabic:1}]
 export const convertRomanNumber = (num : number) => {
-    let arrRomanArabic = [{roman:'M',arabic:1000},{roman:'CM',arabic:900},{roman:'D',arabic:500},{roman:'CD',arabic:400},{roman:'C',arabic:100},{roman:'XC',arabic:90},{roman:'L',arabic:50},{roman:'XL',arabic:40},{roman:'X',arabic:10},{roman:'IX',arabic:9},{roman:'V',arabic:5},{roman:'IV',arabic:4},{roman:'I',arabic:1}]
+   
     let Roman = '';
     
     arrRomanArabic.forEach(element =>{
